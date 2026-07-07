6e5402158a14ac2d5ddf1aa74cbb4893fcde217b
Minae-0714 <lmine0714l@gmail.com>
2026-07-07 12:28:21 -0700

    Add simple Python server for Render Web Service deploy
    Render Web Service needs a running process bound to \$PORT; this serves
    the static landing page without any external dependencies.

---
+++ server.py
@@ -0,0 +1,11 @@
+import http.server
+import os
+import socketserver
+
+PORT = int(os.environ.get("PORT", 8000))
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("0.0.0.0", PORT), Handler) as httpd:
+    print(f"Serving on port {PORT}")
+    httpd.serve_forever()
